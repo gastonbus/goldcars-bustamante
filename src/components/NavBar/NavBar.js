e02448a8a688d8d9843d3ce057465d7857bae002
DESKTOP-GB\Gaston <gaston82@hotmail.com>
2022-06-06 21:27:45 -0300

    Agregado de NavBar incluyendo archivos y carpetas olvidadas en el commit anterior

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import "./NavBar.css";
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <h1>Gold Cars</h1>
+            <ul>
+                <li>
+                    <a href="">Deluxe</a>
+                </li>
+                <li>
+                    <a href="">Sport</a>
+                </li>
+                <li>
+                    <a href="">Family</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
