fdd0d2bd93d8eef3bf308f162994032058f73c06
DESKTOP-GB\Gaston <gaston82@hotmail.com>
2022-07-11 19:37:34 -0300

    Remocion de carpetas para volver a ponerlas luego. Motivo: git no reconoce cambios de nombres de carpetas con letras minusculas modificadas a mayusculas.

--- src/components/Spinner/Spinner.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import "./Spinner.css"
-
-export const Spinner = () => {
-    return <div className="spinner"></div>
-}
--- src/index.js
+++ src/index.js
@@ -2,24 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD6D7-G0Kg0AYUEdjKMN2u2ltYdeBrvgJc",
-  authDomain: "goldcars-bustamante.firebaseapp.com",
-  projectId: "goldcars-bustamante",
-  storageBucket: "goldcars-bustamante.appspot.com",
-  messagingSenderId: "989651937199",
-  appId: "1:989651937199:web:2685419d56e06c51ecb15a"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
+import './Utils/firebaseInitializer.js'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
